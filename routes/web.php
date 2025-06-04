467ee06abfd0444c7daaf5c6452349afd6080f00
ismaelgiovannetti <ismael.giovannetti@heig-vd.ch>
2025-06-04 21:31:46 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ContractController;
 
 // Page d'accueil publique
 Route::get('/', function () {
