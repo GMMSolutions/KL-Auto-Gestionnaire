5189ebc02f05dac0caba428d0ed2c76d484f5583
ismaelgiovannetti <ismael.giovannetti@heig-vd.ch>
2025-06-06 23:25:43 +0200

    Update sale.blade.php

--- resources/views/contracts/templates/sale.blade.php
+++ resources/views/contracts/templates/sale.blade.php
@@ -242,7 +242,7 @@
             </div>
             <div class="warranty-option">
                 <span class="checkbox {{ ($contract->warranty ?? '') === 'quality_1_q5' ? 'checked' : '' }}"></span>
-                <span style="color: #c41e3a; font-weight: bold;">Quality1 Q5 Contre supplément de</span> <span>{{ $contract->warranty_amount ?? '' }}</span>
+                <span style="color: #c41e3a; font-weight: bold;">Quality1 Q5 Contre supplément de</span> <span>CHF {{ number_format($contract->warranty_amount, 2, ',', ' ') }}</span>
             </div>
         </div>
     </div>
