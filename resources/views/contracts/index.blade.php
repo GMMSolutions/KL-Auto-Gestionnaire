d5bcea2857261a070fefd327f749060d2a74945e
ismaelgiovannetti <ismael.giovannetti@heig-vd.ch>
2025-06-07 01:19:04 +0200

    Update index.blade.php

--- resources/views/contracts/index.blade.php
+++ resources/views/contracts/index.blade.php
@@ -41,7 +41,7 @@
     </div>
     <hr>
     
-    <table id="contracts-table" class="table w-100 pb-2">
+    <table id="contracts-table" class="table w-100 pb-5 mb-5">
         <thead>
             <tr>
                 <th>Type</th>
